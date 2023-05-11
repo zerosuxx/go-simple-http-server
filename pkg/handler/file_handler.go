83f6c266a2383b6c4c26a96314c0e26ce2d26d86
Tamas Mohos <zerosuxx@gmail.com>
2023-05-11 17:35:28 +0000

    fix code style

--- pkg/handler/file_handler.go
+++ pkg/handler/file_handler.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/fs"
 	"log"
 	"net/http"
 	"os"
@@ -15,30 +14,16 @@ type FileHandler struct {
 	RootPath string
 }
 
-type Emitter struct {
-	Writer http.ResponseWriter
-  FileInfo fs.FileInfo
-	header string
-}
-
-func (e *Emitter) Emit(data []byte) {
-	if e.header == "" {
-		
-	}
-
-	e.Writer.Write(data)
-}
-
 func handleError(err error, statusCode int, w http.ResponseWriter) {
-  w.WriteHeader(http.StatusNotFound)
-  fmt.Fprint(w, err.Error())
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, err.Error())
 }
 
 func getFileHeader(f *os.File) []byte {
-  buf := make([]byte, 512)
+	buf := make([]byte, 512)
 	_, _ = f.Read(buf)
-  f.Seek(0, 0)
-  return buf
+	f.Seek(0, 0)
+	return buf
 }
 
 func (h FileHandler) Handle(w http.ResponseWriter, r *http.Request) {
@@ -46,31 +31,31 @@ func (h FileHandler) Handle(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("Incoming request path: '%s' | file: '%s'", r.URL.Path, filePath)
 
-  fileInfo, err := os.Stat(filePath)
-  if err != nil {
-    handleError(err, http.StatusNotFound, w)
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		handleError(err, http.StatusNotFound, w)
 
-    return
-  }
-  if fileInfo.IsDir() {
-    err := errors.New("requested file is a directory")
-    handleError(err, http.StatusBadRequest, w)
+		return
+	}
+	if fileInfo.IsDir() {
+		err := errors.New("requested file is a directory")
+		handleError(err, http.StatusBadRequest, w)
 
-    return
-  }
-  
+		return
+	}
+
 	f, err := os.Open(filePath)
 	if err != nil {
-    handleError(err, http.StatusInternalServerError, w)
+		handleError(err, http.StatusInternalServerError, w)
 
 		return
 	}
 	defer f.Close()
 
-  w.Header().Add("Content-Type", http.DetectContentType(getFileHeader(f)))
-  w.Header().Add("Content-Length", strconv.FormatInt(fileInfo.Size(), 10))
-  w.WriteHeader(http.StatusOK)
-  
+	w.Header().Add("Content-Type", http.DetectContentType(getFileHeader(f)))
+	w.Header().Add("Content-Length", strconv.FormatInt(fileInfo.Size(), 10))
+	w.WriteHeader(http.StatusOK)
+
 	buf := make([]byte, 1024)
 	for {
 		n, err := f.Read(buf)
@@ -82,7 +67,7 @@ func (h FileHandler) Handle(w http.ResponseWriter, r *http.Request) {
 			continue
 		}
 		if n > 0 {
-      w.Write(buf[:n])
+			w.Write(buf[:n])
 		}
 	}
 }
